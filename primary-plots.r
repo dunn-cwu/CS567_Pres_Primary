17b9ec12c9fb1ecf7287ad9bcbebd6ac04963997
Andrew Dunn <andrew.dunn@cwu.edu>
2020-02-11 16:21:06 -0800

    Added some R code

--- primary-plots.r
+++ primary-plots.r
@@ -1,1 +1,4 @@
-#TODO: Load data csv file
+setwd("C:\\Users\\Coder\\OneDrive\\Documents\\CS 567 Computational Stats\\CS567_Pres_Primary")
+pdata<-read.csv("data/president_primary_polls.csv")
+
+View(pdata[pdata$party == "DEM", c("question_id", "end_date", "candidate_name", "pct")])
