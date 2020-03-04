289a8fefb0ea44ffc92f37dcb7b7519ea8797e78
Andrew Dunn <andrew.dunn@cwu.edu>
2020-03-03 18:10:36 -0800

    Small changes to script

--- primary-plots.r
+++ primary-plots.r
@@ -38,7 +38,7 @@ pdata_dem<-subset(pdata_dem, fte_grade == 'A+' | fte_grade == 'A' | fte_grade ==
 pdata_dem$end_date <- as.Date(pdata_dem$end_date, format = "%m/%d/%y")
 
 # Filter out date range
-pdata_dem<-pdata_dem[pdata_dem$end_date >= "2020-01-01",]
+pdata_dem<-pdata_dem[pdata_dem$end_date >= "2020-02-01",]
 
 # Sort dataframe by poll end date, question id, and candidate id
 pdata_dem<-pdata_dem[order(pdata_dem$end_date, pdata_dem$question_id, pdata_dem$candidate_id),]
@@ -59,29 +59,22 @@ bloomberg<-filterOutCandidate(pdata_dem, 13289, poll)
 # Get poll data for buttigieg
 buttigieg<-filterOutCandidate(pdata_dem, 13345, poll)
 
-# Get poll data for buttigieg
+# Get poll data for warren
 warren<-filterOutCandidate(pdata_dem, 13258, poll)
 
-#spline.d <- as.data.frame(spline(sanders$end_date, sanders$pct))
-#spline.d$x <- as.Date(spline.d$x, origin = "1970-01-01")
 
 # Set plot line size
 lineSize<-1
 d_election=data.frame(date=as.Date(c("2020-02-03", "2020-02-11", "2020-02-22", "2020-02-29")), event=c("Iowa Caucus", "New Hampshire Primary", "Nevada Caucus", "South Carolina Primary"))
 d_debate=data.frame(date=as.Date(c("2020-02-07", "2020-02-19")), event=c("8th Presidential Debate", "9th Presidential Debate"))
-# Plot sanders polls
+
+# Plot polls
 ggplot() +
   geom_line(data=sanders, aes(x=end_date, y=pct, color="san"), size = lineSize) +
-  # geom_point(data=sanders, aes(x=end_date, y=pct, group=1)) +
   geom_line(data=biden, aes(x=end_date, y=pct, color="bid"), size = lineSize) +
-  # geom_point(data=biden, aes(x=end_date, y=pct, group=1)) +
   geom_line(data=bloomberg, aes(x=end_date, y=pct, color="bloom"), size = lineSize) +
   geom_line(data=buttigieg, aes(x=end_date, y=pct, color="butt"), size = lineSize) +
   geom_line(data=warren, aes(x=end_date, y=pct, color="war"), size = lineSize) +
-  #geom_vline(xintercept = "2020-02-03", linetype="dotted", color = "blue", size=1.5) +
-  #geom_vline(xintercept = as.numeric(as.Date("2020-02-03")), size=1, linetype=4) +
-  #geom_vline(xintercept = as.numeric(as.Date("2020-02-11")), size=1, linetype=4) +
-  #geom_vline(xintercept = as.numeric(as.Date("2020-02-22")), size=1, linetype=4) +
 
   geom_vline(data=d_election, mapping=aes(xintercept=date), color="red") +
   geom_text(data=d_election, mapping=aes(x=date, y=0, label=event), size=4, angle=90, vjust=-0.4, hjust=0.1) +
@@ -91,7 +84,7 @@ ggplot() +
   geom_text(data=d_debate, mapping=aes(x=date, y=0, label=date), size=4, angle=90, vjust=1.1, hjust=0.15) +
 
   scale_color_discrete(name = "Candidate", labels = c("Biden", "Bloomberg", "Buttigieg", "Sanders", "Warren")) +
-  #geom_line(data = spline.d, aes(x = x, y = y), size = 1)
+
   ggtitle("Candidate Support Over Time") +
   labs(y="Percent", x="Date") +
   theme(legend.position="bottom")
