ab69aa77c294905ccaa3c89e0a56bcb2d3aff9ff
Andrew Dunn <andrew.dunn@cwu.edu>
2020-03-04 16:12:35 -0800

    Added scatter plot with regression

--- primary-plots.r
+++ primary-plots.r
@@ -9,7 +9,7 @@ theme_set(
     theme(legend.position = "top")
 )
 
-filterOutCandidate<-function(df, canID, pollsterID) {
+filterOutCandidate<-function(df, canID, pollsterID, useAggr) {
   if (pollsterID == 0)
   {
     df<-df[df$candidate_id == canID, c("end_date", "pct")]
@@ -18,7 +18,10 @@ filterOutCandidate<-function(df, canID, pollsterID) {
   {
     df<-df[df$candidate_id == canID & df$pollster_id == pollsterID, c("end_date", "pct")]
   }
-  df<-setNames(aggregate(df[, 2], list(df$end_date), mean), c("end_date", "pct"))
+  if (useAggr == TRUE)
+  {
+    df<-setNames(aggregate(df[, 2], list(df$end_date), mean), c("end_date", "pct"))
+  }
   return (df)
 }
 
@@ -31,9 +34,6 @@ pdata_dem<-pdata[pdata$party=="DEM", c("question_id", "poll_id", "pollster_id", 
 # Replace blank cells with NA
 pdata_dem[pdata_dem == ""] = NA
 
-# Filter out all polls that are rated worse than A-
-#pdata_dem<-subset(pdata_dem, fte_grade == 'A+' | fte_grade == 'A' | fte_grade == 'A-')
-
 # Replace data strings with date data type
 pdata_dem$end_date <- as.Date(pdata_dem$end_date, format = "%m/%d/%y")
 
@@ -42,25 +42,29 @@ pdata_dem<-pdata_dem[pdata_dem$end_date >= "2020-01-01",]
 
 # Sort dataframe by poll end date, question id, and candidate id
 pdata_dem<-pdata_dem[order(pdata_dem$end_date, pdata_dem$question_id, pdata_dem$candidate_id),]
+
+# Filter out all polls that are rated worse than A-
+pdata_dem<-subset(pdata_dem, fte_grade == 'A+' | fte_grade == 'A' | fte_grade == 'A-')
+
 View(pdata_dem)
 
 # Use poll 1102 or 0 for all polls
 poll<-0
 
 # Get poll data for sanders
-sanders<-filterOutCandidate(pdata_dem, 13257, poll)
+sanders<-filterOutCandidate(pdata_dem, 13257, poll, TRUE)
 
 # Get poll data for biden
-biden<-filterOutCandidate(pdata_dem, 13256, poll)
+biden<-filterOutCandidate(pdata_dem, 13256, poll, TRUE)
 
 # Get poll data for biden
-bloomberg<-filterOutCandidate(pdata_dem, 13289, poll)
+bloomberg<-filterOutCandidate(pdata_dem, 13289, poll, TRUE)
 
 # Get poll data for buttigieg
-buttigieg<-filterOutCandidate(pdata_dem, 13345, poll)
+buttigieg<-filterOutCandidate(pdata_dem, 13345, poll, TRUE)
 
 # Get poll data for warren
-warren<-filterOutCandidate(pdata_dem, 13258, poll)
+warren<-filterOutCandidate(pdata_dem, 13258, poll, TRUE)
 
 
 # Set plot line size
@@ -86,5 +90,28 @@ ggplot() +
   scale_color_discrete(name = "Candidate", labels = c("Biden", "Bloomberg", "Buttigieg", "Sanders", "Warren")) +
 
   ggtitle("Candidate Support Over Time") +
+  labs(y="Percent", x="Date") +
+  theme(legend.position="bottom")
+
+sandersScatter<-filterOutCandidate(pdata_dem, 13257, poll, FALSE)
+bidenScatter<-filterOutCandidate(pdata_dem, 13256, poll, FALSE)
+warrenScatter<-filterOutCandidate(pdata_dem, 13258, poll, FALSE)
+
+ggplot() + 
+  geom_point(aes(x=end_date, y=pct, color="Sanders"), data=sandersScatter) +
+  geom_point(aes(x=end_date, y=pct, color="Biden"), data=bidenScatter) +
+  geom_point(aes(x=end_date, y=pct, color="Warren"), data=warrenScatter) +
+  geom_smooth(aes(x=end_date, y=pct, color="Sanders"), data=sandersScatter, method="auto") +
+  geom_smooth(aes(x=end_date, y=pct, color="Biden"), data=bidenScatter, method="auto") +
+  geom_smooth(aes(x=end_date, y=pct, color="Warren"), data=warrenScatter, method="auto") +
+  
+  geom_vline(data=d_election, mapping=aes(xintercept=date), color="darkred") +
+  geom_text(data=d_election, mapping=aes(x=date, y=0, label=event), size=4, angle=90, vjust=-0.4, hjust=0.1) +
+  geom_text(data=d_election, mapping=aes(x=date, y=0, label=date), size=4, angle=90, vjust=1.1, hjust=0.15) +
+  geom_vline(data=d_debate, mapping=aes(xintercept=date), color="blue") +
+  geom_text(data=d_debate, mapping=aes(x=date, y=0, label=event), size=4, angle=90, vjust=-0.4, hjust=0.1) +
+  geom_text(data=d_debate, mapping=aes(x=date, y=0, label=date), size=4, angle=90, vjust=1.1, hjust=0.15) +
+  
+  ggtitle("Sanders, Biden, and Warren Poll Support - A Rated Polls") +
   labs(y="Percent", x="Date") +
   theme(legend.position="bottom")
